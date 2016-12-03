9e51042e15321c05ca086517ed36dd2470bef855
DDChiang <ddeechng@gmail.com>
2016-12-03 13:48:20 -0800

    test to see if items inside build folder are excluded in git commits

--- build/appConfig/index.js
+++
@@ -1,3 +0,0 @@
-// list your email and passwords into here
-// if commiting, remember to put this file into your .gitignore!
-module.exports = [{ login: 'random@gmail.com', password: 'randomPassword!' }, { login: 'random@gmail.com', password: 'randomPassword!' }];
