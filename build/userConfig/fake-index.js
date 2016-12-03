bfa25ef28583ca3af92120d952b76af78b7fab96
DDChiang <ddeechng@gmail.com>
2016-12-03 13:49:06 -0800

    clean out build folder

--- build/userConfig/fake-index.js
+++
@@ -1,4 +0,0 @@
-// Create a "index.js" file in the same folder and COPY ALL THE CONTENTS over.
-// Add/Replace email and password combos into here
-// if commiting, remember to make sure "src/userConfig/index.js" is placed into your .gitignore!
-module.exports = [{ login: 'random@gmail.com', password: 'randomPassword!' }, { login: 'random@gmail.com', password: 'randomPassword!' }];
